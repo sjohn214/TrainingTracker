03aa6e26f1002867568154c4370f6b04f29a7ce9
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-02-24 09:48:19 -0500

    Add api route

---
+++ Develop/routes/api.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const Workout = require("../models/workout");
+
+//post route to create workouts
+
+//put route to
+
+//get round
+
+//delete route 
+
+module.exports = router;
