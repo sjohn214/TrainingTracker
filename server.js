fb486232ba37343f2b67fb09da6bf9423bfe02f0
Shannon L Johnson-Martin <shannonljohnson-martin@Shannons-MBP.attlocal.net>
2021-02-27 14:21:42 -0500

    Add dotenv to server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-
+require ("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
